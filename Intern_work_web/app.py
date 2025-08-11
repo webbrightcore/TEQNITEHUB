b80684c59a9f9c67b8d171cb3ecc0d7681b79b58
webbrightcore <webbrightcore@gmail.com>
2025-08-11 17:07:16 +0500

    Update app.py

--- Intern_work_web/app.py
+++ Intern_work_web/app.py
@@ -635,7 +635,7 @@ st.markdown("---")
 
 # Typing animation HTML
 components.html("""
-<div id="typed-text-container" style="display: flex; justify-content: center; margin-top: 0; margin-bottom: 0;">
+<div id="typed-text-container" style="display: flex; margin-top: 0; margin-bottom: 0;">
   <div id="typed-text" style="
       font-family: 'Helvetica Neue', sans-serif;
       font-weight: 700;
@@ -1016,3 +1016,4 @@ st.markdown("""
     </div>
 </div>
 """, unsafe_allow_html=True)
+
